69a63f0acfdf320f2e595cf77bcff464cff97fe2
ptonini <pedro.tonini@hotmail.com>
2016-05-24 08:34:04 -0300

    commit

--- battuta/apps/preferences/static/preferences/preferences.js
+++ battuta/apps/preferences/static/preferences/preferences.js
@@ -1,13 +1,18 @@
-function buildPreferencesForm() {
+function buildPreferencesContainer() {
 
     var preferencesContainer = $('#preferences_container');
     
     var divRow = $('<div>').attr('class', 'row');
     var divFormGroup = $('<div>').attr('class', 'form-group');
-    var divCol2 = $('<div>').attr('class', 'col-md-1 col-xs-2 report_field_right');
-    var divCol3 = $('<div>').attr('class', 'col-md-3 col-xs-3 report_field_left');
+    var divCol2 = $('<div>').attr('class', 'col-md-3 col-xs-3');
     var divCol12 = $('<div>').attr('class', 'col-md-12');
+    var alterButton = $('<button>').attr('class', 'btn btn-default btn-sm');
     var inputField = $('<input>').attr({type: 'text', class: 'form-control input-sm'});
+    var booleanField = $('<select>').attr({class: 'select form-control input-sm'}).append(
+        $('<option>').html('True').val(true),
+        $('<option>').html('False').val(false)
+    );
+    var fieldLabel = $('<label>').css({'font-size': '13px', padding: '6px 0'}).data('toggle', 'tooltip');
     
     $.ajax({
         url: '',
@@ -15,28 +20,81 @@ function buildPreferencesForm() {
         dataType: 'json',
         data: {action: 'preferences'},
         success: function (data) {
-            Object.keys(data).forEach(function (key) {
-                preferencesContainer.append(
-                    divRow.clone().append(
-                        divCol12.clone().append(
-                            $('<h4>').html(key)
-                        )
-                    )
-                );
-                $.each(data[key]['items'], function(index, item) {
+            $.each(data, function (index, item_group) {
+                var itemGroupId = 'item_group_' + item_group.id;
+                var itemGroupSeparatorId = itemGroupId + '_separator';
+                var itemGroupSelector = '#' + itemGroupId;
+                if ($(itemGroupSelector).length == 0) {
                     preferencesContainer.append(
-                        divRow.clone().append(
-                            divCol3.clone().append(
-                                $('<label>').attr('for', item.name).html(item.name)
-                            ),
-                            divCol2.clone().append(
-                                divFormGroup.clone().append(
-                                    inputField.clone().attr('id', item.name)
+                        $('<div>').attr('id', itemGroupId).append(
+                            divRow.clone().append(
+                                divCol2.clone().append(
+                                    $('<h4>').html(item_group.name)
+                                ),
+                                divCol2.clone().addClass('edit_mode').css('display', 'none').append(
+                                    alterButton.clone().html('Edit'),
+                                    $('<span>').html('&nbsp;'),
+                                    alterButton.clone().html('Remove')
                                 )
                             )
                         )
-                    )
+                    );
+                }
+                var itemGroup = $(itemGroupSelector);
+                $.each(item_group['items'], function(index, item) {
+
+                    var itemId = 'item_' + item.id;
+                    var itemIdSelector = '#' + itemId;
+                    var itemField = null;
+                    var columnClass = null;
+                    switch (item.data_type) {
+                        case 'str':
+                            itemField = inputField.clone();
+                            columnClass = 'col-md-3';
+                            break;
+                        case 'bool':
+                            itemField = booleanField;
+                            columnClass = 'col-md-1';
+                            break;
+                        case 'int':
+                        case 'float':
+                            itemField = inputField.clone();
+                            columnClass = 'col-md-1';
+                            break;
+                    }
+
+                    if ($(itemIdSelector).length == 0) {
+                        itemGroup.append(
+                            divRow.clone().append(
+                                divCol2.clone().append(
+                                    fieldLabel.clone().html(item.name + ':').attr({
+                                        for: item.name,
+                                        title: item.description
+                                    })
+                                ),
+                                $('<div>').addClass(columnClass).append(
+                                    divFormGroup.clone().append(
+                                        itemField.attr('id', itemId)
+                                    )
+                                ),
+                                divCol2.clone().addClass('edit_mode').css('display', 'none').append(
+                                    alterButton.clone().html('Edit'),
+                                    $('<span>').html('&nbsp;'),
+                                    alterButton.clone().html('Remove')
+                                )
+                            )
+                        )
+                    }
+                    $(itemIdSelector).val(item.value)
                 });
+                if ($('#' + itemGroupSeparatorId).length == 0) {
+                    itemGroup.append($('<hr>').attr('id', itemGroupSeparatorId))
+                }
+                if ($('#item_group').find('option[value="' + item_group.id + '"]').length == 0) {
+                    $('#item_group').append(
+                        $('<option>').html(item_group.name).val(item_group.id)
+                    )
+                }
 
             });
         }
@@ -45,32 +103,13 @@ function buildPreferencesForm() {
 
 $(document).ready(function () {
 
-    var itemDialog = $('#item_dialog');
-    var itemForm = $('#item_form');
-    var itemGroupDialog = $('#item_group_dialog');
-    var itemGroupForm = $('#item_group_form');
-    var alertDialog = $('#alert_dialog');
+    var itemOrGroupForm = $('#item_or_group_form');
+    var itemOrGroupDialog = $('#item_or_group_dialog');
+    var itemOrGroupDialogHeader = $('#item_or_group_dialog_header');
     
-    buildPreferencesForm();
-    
-    itemDialog.dialog({
-        autoOpen: false,
-        modal: true,
-        show: true,
-        hide: true,
-        width: 400,
-        dialogClass: 'no_title',
-        buttons: {
-            Save: function (){
-                itemForm.submit()
-            },
-            Cancel: function (){
-                itemDialog.dialog('close');
-            }
-        }
-    });
+    buildPreferencesContainer();
 
-    itemGroupDialog.dialog({
+    itemOrGroupDialog.dialog({
         autoOpen: false,
         modal: true,
         show: true,
@@ -78,69 +117,60 @@ $(document).ready(function () {
         dialogClass: 'no_title',
         buttons: {
             Save: function (){
-                itemGroupForm.submit()
+                itemOrGroupForm.submit()
             },
             Cancel: function (){
-                itemGroupDialog.dialog('close');
+                itemOrGroupDialog.dialog('close');
             }
         }
     });
 
-    $('#add_item').click(function(event) {
+    $('.open_item_dialog').click(function(event) {
         event.preventDefault();
-        itemForm.data('id', '');
-        itemDialog.dialog('open');
+        var itemOnly = $('.item_only');
+        itemOrGroupForm.removeData().find('*').val('');
+        itemOrGroupDialog.dialog('open');
+        switch ($(this).data('action')) {
+            case 'add_item':
+                itemOnly.show();
+                itemOrGroupForm.data('id', '').data('type', 'item');
+                itemOrGroupDialog.dialog('option', 'width', 400);
+                break;
+            case 'add_item_group':
+                itemOnly.hide();
+                itemOrGroupForm.data('id', '').data('type', 'item_group');
+                itemOrGroupDialog.dialog('option', 'width', 330);
+                break;
+        }
+        itemOrGroupDialogHeader.html($(this).html())
     });
 
-    $('#add_item_group').click(function(event) {
-        event.preventDefault();
-        itemGroupForm.data('id', '');
-        itemGroupDialog.dialog('open');
-    });
-
-    itemForm.submit(function(event) {
+    itemOrGroupForm.submit(function(event) {
         event.preventDefault();
         $.ajax({
             url: '',
             type: 'POST',
             dataType: 'json',
             data: {
-                action: 'save_item',
-                id: itemForm.data('id'),
-                name: $('#item_name').val(),
-                description: $('#item_description').val(),
+                action: 'save',
+                type: itemOrGroupForm.data('type'),
+                id: itemOrGroupForm.data('id'),
+                name: $('#item_or_group_name').val(),
+                description: $('#item_or_group_description').val(),
                 data_type: $('#item_data_type').val(),
                 item_group: $('#item_group').val()
             },
             success: function (data) {
-                if (data.result == 'fail') {
-                    alertDialog.html('<strong>' + data.msg + '</strong>').dialog('open');
+                if (data.result == 'ok') {
+                    buildPreferencesContainer();
+                    $('#item_or_group_dialog').dialog('close');
+                }
+                else if (data.result == 'fail') {
+                    $('#alert_dialog').html('<strong>' + data.msg + '</strong>').dialog('open');
                 }
 
             }
         });
-    });
-
-    itemGroupForm.submit(function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: '',
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                action: 'save_group',
-                id: itemGroupForm.data('id'),
-                name: $('#item_group_name').val(),
-                description: $('#item_group_description').val()
-            },
-            success: function (data) {
-                if (data.result == 'fail') {
-                    alertDialog.html('<strong>' + data.msg + '</strong>').dialog('open');
-                }
-
-            }
-        });
-
     })
 
 });
--- battuta/apps/preferences/views.py
+++ battuta/apps/preferences/views.py
@@ -2,9 +2,7 @@ import json
 
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import View
-from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, Http404
-from django.conf import settings
 from django.forms import model_to_dict
 
 from models import Item, ItemGroup
@@ -19,12 +17,13 @@ class PreferencesView(View):
             return render(request, 'preferences/preferences.html')
         else:
             if request.GET['action'] == 'preferences':
-                data = dict()
+                data = list()
                 for item_group in ItemGroup.objects.all():
-                    print item_group.name
-                    data[item_group.name] = {'description': item_group.description, 'items': list()}
+                    item_group_dict = model_to_dict(item_group)
+                    item_group_dict['items'] = list()
                     for item in item_group.item_set.all().values():
-                        data[item_group.name]['items'].append(item)
+                        item_group_dict['items'].append(item)
+                    data.append(item_group_dict)
             else:
                 raise Http404('Invalid action')
             return HttpResponse(json.dumps(data), content_type="application/json")
@@ -32,26 +31,22 @@ class PreferencesView(View):
     @staticmethod
     def post(request):
 
-        if request.POST['action'] == 'save_item':
-            if request.POST['id'] == '':
-                item = Item()
+        if request.POST['action'] == 'save_item_or_group':
+            if request.POST['type'] == 'item':
+                preference_object_class = Item
+                form_class = ItemForm
+            elif request.POST['type'] == 'item_group':
+                preference_object_class = ItemGroup
+                form_class = ItemGroupForm
             else:
-                item = get_object_or_404(Item(), pk=request.POST['id'])
-            post_data = dict(request.POST.iteritems())
-            form = ItemForm(post_data or None, instance=item)
-            if form.is_valid():
-                form.save(commit=True)
-                data = {'result': 'ok'}
-            else:
-                data = {'result': 'fail', 'msg': str(form.errors)}
+                raise Http404('Invalid type')
 
-        elif request.POST['action'] == 'save_group':
             if request.POST['id'] == '':
-                item_group = ItemGroup()
+                preference_object = preference_object_class()
             else:
-                item_group = get_object_or_404(ItemGroup(), pk=request.POST['id'])
+                preference_object = get_object_or_404(preference_object_class(), pk=request.POST['id'])
             post_data = dict(request.POST.iteritems())
-            form = ItemGroupForm(post_data or None, instance=item_group)
+            form = form_class(post_data or None, instance=preference_object)
             if form.is_valid():
                 form.save(commit=True)
                 data = {'result': 'ok'}
