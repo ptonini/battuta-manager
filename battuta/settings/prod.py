54dc03d421ea41c52f64542cf86a6349d9709e23
ptonini <pedro.tonini@hotmail.com>
2016-06-24 14:02:11 -0300

    commit

--- battuta/settings/prod.py
+++ battuta/settings/prod.py
@@ -6,8 +6,6 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 DEBUG = False
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
