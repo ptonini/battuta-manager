5a41431b35975271d4b7900065d5e0a17043a726
ptonini <pedro.tonini@hotmail.com>
2016-07-13 16:57:32 -0300

    commit

--- battuta/apps/preferences/views.py
+++ battuta/apps/preferences/views.py
@@ -1,5 +1,4 @@
 import json
-import copy
 
 from django.shortcuts import render
 from django.views.generic import View
@@ -7,7 +6,6 @@ from django.http import HttpResponse, Http404
 from django.conf import settings
 
 from models import Item
-from . import default_prefs
 
 
 class PreferencesView(View):
@@ -18,32 +16,28 @@ class PreferencesView(View):
             return render(request, 'preferences/preferences.html')
         else:
             if request.GET['action'] == 'preferences':
-                data = list()
-
-                for item_group in list(default_prefs):
-
-                    data.append(item_group.copy())
-
-                # for i, item_group in enumerate(data):
-                #     for j, item in enumerate(item_group['items']):
-                #         try:
-                #             data[i]['items'][j]['value'] = Item.objects.get(name=item['name']).value
-                #         except Item.DoesNotExist:
-                #             pass
-
+                data = list(settings.DEFAULT_PREFERENCES)
+                for i, item_group in enumerate(data):
+                    for j, item in enumerate(item_group['items']):
+                        try:
+                            data[i]['items'][j]['value'] = Item.objects.get(name=item['name']).value
+                        except Item.DoesNotExist:
+                            pass
             else:
                 raise Http404('Invalid action')
             return HttpResponse(json.dumps(data), content_type="application/json")
 
     @staticmethod
     def post(request):
-        if request.POST['action'] == 'save':
-            print default_prefs
-            for k, v in json.loads(request.POST['item_values']).iteritems():
-                i, created = Item.objects.get_or_create(name=k)
-                i.value = v
-                i.save()
-
-        data = {'result': 'ok'}
-
+        if 'action' in request.POST:
+            if request.POST['action'] == 'save':
+                for key, value in json.loads(request.POST['item_values']).iteritems():
+                    item, created = Item.objects.get_or_create(name=key)
+                    item.value = value
+                    item.save()
+                data = {'result': 'ok'}
+            else:
+                raise Http404('Invalid action')
+        else:
+            raise Http404('Invalid request')
         return HttpResponse(json.dumps(data), content_type="application/json")
