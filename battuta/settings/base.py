75ea91a7ad69569f41b1d42dfe3e242c00590b69
ptonini <pedro.tonini@hotmail.com>
2016-10-14 09:06:40 -0300

    commit

--- battuta/settings/base.py
+++ battuta/settings/base.py
@@ -80,7 +80,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 LOGIN_URL = '/'
 
 DATA_DIR = '/opt/ans_data'
-FACTS_DIR = os.path.join(DATA_DIR, 'facts')
 PLAYBOOK_DIR = os.path.join(DATA_DIR, 'playbooks')
 USERDATA_DIR = os.path.join(DATA_DIR, 'userdata')
 
