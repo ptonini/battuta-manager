0a1471b0ef7a39bf4d72e2eac8b6724ee18ffc8a
Pedro Tonini <pedro.tonini@hotmail.com>
2019-01-23 21:50:19 -0200

    commit

--- extras/scripts/bootstrap.sh
+++ extras/scripts/bootstrap.sh
@@ -7,19 +7,18 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password_again passwo
 apt update
 #apt -y upgrade
 apt -y install curl software-properties-common dirmngr
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
+apt-key adv --no-tty --recv-keys --keyserver keyserver.ubuntu.com 0xF1656F24C74CD1D8
 add-apt-repository 'deb http://mirror.ufscar.br/mariadb/repo/10.3/debian stretch main'
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -sL https://bootstrap.pypa.io/get-pip.py | sudo -E python -
-apt install -y build-essential python3-dev mariadb-server libmariadbclient-dev-compat libssl-dev libffi-dev git nodejs libcurl4-openssl-dev
+apt install -y build-essential python3-dev mariadb-server libssl-dev libffi-dev git nodejs libcurl4-openssl-dev
 apt remove -y python3-httplib2 python3-pycurl
-apt autoremove
 pip install requests
-pip install mysqlclient
 pip install -r /opt/battuta/requirements.txt
 npm install -g bower
 mysql -u root -pbattuta --execute='CREATE DATABASE battuta CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'
 mysql -u root -pbattuta --execute='GRANT ALL PRIVILEGES ON battuta.* TO "battuta"@"%"  IDENTIFIED BY "battuta";'
 cd /opt/battuta
 bower install --allow-root
+apt -y autoremove
 ./manage.py migrate
